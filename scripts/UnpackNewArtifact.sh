0b9917780cfa87c5f582a661b35322ac67430cf9
Frank Schlesinger <fschlesinger@isdeblnnm275.gs24.com>
2017-06-27 15:28:46 +0200

    made aplication.log writable and set db timeout much higher

--- scripts/UnpackNewArtifact.sh
+++ scripts/UnpackNewArtifact.sh
@@ -2,3 +2,4 @@
 cd /tmp
 unzip *zip -d play-scala-application
 sudo chmod a+w play-scala-application/play-scala-0.1.0-SNAPSHOT
+sudo chmod a+w play-scala-application/play-scala-0.1.0-SNAPSHOT/logs/application.log
